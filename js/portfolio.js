db3c68545cd9736feebdfdcd7d1bab19eb7a5df9
Nejc Janša <jansa.nejc@gmail.com>
2013-06-29 12:52:55 +0200

    Portfolio
    Dodan portfolio, prva verzija brez sprememb besedila

---
+++ js/portfolio.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	$("#portfolio").booklet({
+		width:  '100%',
+		height: '100%',
+		autoCenter: true,
+		closed: true,
+        covers: true,
+		pageNumbers: false,
+		easing:  null,
+        easeIn:  null,
+        easeOut: null,
+		shadows: false
+
+	});	
+});
